e1ef672a84c0a0bb4ce493347bfdb1f693796f76
prateek <stupidcoder22@gmail.com>
2022-04-16 10:06:37 +0530

    create new post completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Topbar from "./Components/Topbar/Topbar";
 import Home from "./pages/home/Home";
 import { Single } from "./pages/single/Single";
+import { Write } from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Single />
+      <Write />
     </>
   );
 }
