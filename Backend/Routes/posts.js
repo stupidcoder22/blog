46dfec79392fbee4192030c1cd4aa314b95289ec
prateek <stupidcoder22@gmail.com>
2022-04-17 08:57:29 +0530

    get post api done

--- Backend/Routes/posts.js
+++ Backend/Routes/posts.js
@@ -14,33 +14,66 @@ router.post("/", async (req, res) => {
 });
 
 //! UPDATE NEW POST
-router.delete("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    const user = await User.findById(req.params.id);
-    if (user) {
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.username === req.body.username) {
       try {
-        console.log("inside try");
-        await Post.deleteMany({ username: req.body.username });
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted");
+        const updatepost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatepost);
       } catch (error) {
-        return res.status(500).json(error);
+        res.status(500).json(error);
       }
     } else {
-      res.status(404).json("User not Found");
+      res.status(401).json("You are not allowed to update");
     }
-  } else {
-    res.status(401).json("you can delete only through your account");
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
 // ! DELETE POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("post deleted successfully");
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json("You are not allowed to delete");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // !GET POST
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    res.status(200).json(user);
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// !GET ALL POST
+router.get("/", async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
   } catch (error) {
     res.status(500).json(error);
   }
