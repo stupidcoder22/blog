2651d7d0a8bfb5b1464a91bc5f2af26ee425ca7a
prateek <stupidcoder22@gmail.com>
2022-04-18 09:18:34 +0530

    small update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,11 +4,12 @@ const Header = () => {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitlessm">React and Node</span>
+        <span className="headerTitlessm">Welcome To My</span>
         <span className="headerTitleslg">BLOG</span>
       </div>
-      <img className="headerImg"
-        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/235648/pexels-photo-235648.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         alt=""
       />
     </div>
